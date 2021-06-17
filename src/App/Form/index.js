cad9a647d8c4d49b7797d8c70a696897188e4c53
Katarzyna Napierala <k_napierala@op.pl>
2021-06-17 17:08:13 +0200

    Form changed to controlled component

--- src/App/Form/index.js
+++ src/App/Form/index.js
@@ -1,17 +1,41 @@
+import { useState } from "react";
 import "./style.css";
 
 const Form = (props) => {
+    const [amount, setAmount] = useState("");
+    const [currency, setCurrency] = useState("EUR");
+
     return (
         <form className="form" action="https://postman-echo.com/get">
             <p>
-                <label><span className="form__labelText">Podaj kwotę w PLN*</span>
-                    <input className="form__field" name="PLN" type="number" step="any" min="0" autoFocus
-                        required />
+                <label>
+                    <span className="form__labelText">
+                        Podaj kwotę w PLN*
+                    </span>
+                    <input
+                        value={amount}
+                        onChange={({ target }) => setAmount(target.value)}
+                        className="form__field"
+                        name="PLN"
+                        type="number"
+                        step="any"
+                        min="0"
+                        autoFocus
+                        required
+                    />
                 </label>
             </p>
             <p>
-                <label><span className="form__labelText">Wybierz walutę</span>
-                    <select className="form__field" name="currency">
+                <label>
+                    <span className="form__labelText">
+                        Wybierz walutę
+                    </span>
+                    <select
+                        value={currency}
+                        onChange={({ target }) => setCurrency(target.value)}
+                        className="form__field"
+                        name="currency"
+                    >
                         <option value="EUR">Euro</option>
                         <option value="USD">Dolar amerykański</option>
                         <option value="GBP">Funt brytyjski</option>
