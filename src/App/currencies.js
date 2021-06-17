783a3f0e20f39caff1cf8424a48534d5a712d2b1
Katarzyna Napierala <k_napierala@op.pl>
2021-06-17 17:33:56 +0200

    currencies module with array of currencies added

---
+++ src/App/currencies.js
@@ -0,0 +1,19 @@
+const currencies = [
+    {
+        short: "EUR",
+        name: "Euro",
+        rate: 4.57
+    },
+    {
+        short: "USD",
+        name: "Dolar Amerykański",
+        rate: 4.57
+    },
+    {
+        short: "GBP",
+        name: "Funt Brytyjski",
+        rate: 4.57
+    },
+];
+
+export { currencies };
