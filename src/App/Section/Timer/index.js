575afc902f6163eb71d6d4674cafb4184e69f237
Katarzyna Napierala <k_napierala@op.pl>
2021-06-25 13:03:56 +0200

    formattedDate() added

--- src/App/Section/Timer/index.js
+++ src/App/Section/Timer/index.js
@@ -1,10 +1,21 @@
-import "./useCurrentDate";
+import useCurrentDate from "./useCurrentDate";
 import "./style.css";
 
 const Timer = (props) => {
+    const date = useCurrentDate();
+
+    const formattedDate = date.toLocaleString(undefined, {
+        weekday: "long",
+        day: "numeric",
+        month: "long",
+        hour: "2-digit",
+        minute: "2-digit",
+        second: "2-digit"
+    });
+
     return (
-        <p>
-            Dzisiaj jest ...
+        <p className="timer">
+            Dzisiaj jest {formattedDate}
         </p>
     );
 };
