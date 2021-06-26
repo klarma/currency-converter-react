7bdff1072e712afbae07d9794cfc7e8827f899c7
Katarzyna Napierala <k_napierala@op.pl>
2021-06-26 22:18:41 +0200

    unused classNames container and header deleted

--- src/App/Header/index.js
+++ src/App/Header/index.js
@@ -1,7 +1,7 @@
 import "./style.css";
 
 const Header = () => (
-    <header className="header">
+    <header>
         <h1 className="header__title">Kantor wymiany walut</h1>
         <p>Pole oznaczone* jest wymagane</p>
     </header>
