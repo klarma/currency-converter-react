96c67ca30691ca96de50fddc46033dfaaac2ada6
Katarzyna Napierala <k_napierala@op.pl>
2021-08-11 10:14:08 +0200

    styled-components installed, theme.js added

---
+++ src/theme.js
@@ -0,0 +1,12 @@
+export const theme = {
+    color: {
+        silverChalice: "#aaa",
+        silver: "#ccc",
+        gallery: "#eee",
+        white: "#fff",
+        cabaret: "#D75067",
+    },
+    breakpoint: {
+        mobileMax: 767,
+    },
+};
