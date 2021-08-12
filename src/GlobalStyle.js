4140c47194aa608da720b655ea51bb1d211f78e4
Katarzyna Napierala <k_napierala@op.pl>
2021-08-12 15:11:04 +0200

    GlobalStyle.js created

---
+++ src/GlobalStyle.js
@@ -0,0 +1,26 @@
+import { createGlobalStyle } from "styled-components";
+import background from "./money.jpg";
+
+export const GlobalStyle = createGlobalStyle`
+    html {
+        box-sizing: border-box;
+        scroll-behavior: smooth;
+    }
+  
+    *, ::after, ::before {
+        box-sizing: inherit;
+    }
+  
+    body {
+        max-width: 700px;
+        margin: 0 auto;
+        background-image: url("${background}");
+        background-position: 50%;
+        background-size: cover;
+        padding: 0 20px;
+        color: ${({ theme }) => theme.color.gallery};
+        font-family: "Roboto", sans-serif;
+        text-align: center;
+    }
+`;
+
