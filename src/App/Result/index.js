45491c2d2f4ab0f5f75da6107587473672c8663c
Katarzyna Napierala <k_napierala@op.pl>
2021-09-13 11:41:59 +0200

    styled-component Note added in Result

--- src/App/Result/index.js
+++ src/App/Result/index.js
@@ -1,17 +1,23 @@
-import { StyledResult } from "./styled";
+import { StyledResult, Note } from "./styled";
+import {useRatesData} from "../useRatesData";
 
-const Result = ({ result }) => (
-    <StyledResult>
-        {!!result && (
-            <>
-                {result.sourceAmount.toFixed(2)}&nbsp;PLN&nbsp;=
-                {" "}
-                <strong>
-                    {result.targetAmount.toFixed(2)}&nbsp;{result.currency}
-                </strong>
-            </>
-        )}
-    </StyledResult>
-);
+const Result = ({ result }) => {
+    const {date} = useRatesData();
+
+    return (
+        <StyledResult>
+            {!!result && (
+                <>
+                    {result.sourceAmount.toFixed(2)}&nbsp;PLN&nbsp;=
+                    {" "}
+                    <strong>
+                        {result.targetAmount.toFixed(2)}&nbsp;{result.currency}
+                    </strong>
+                </>
+            )}
+            <Note>Kursy walut pobierane są z Europejskiego Banku Centralnego. Aktualne na dzień {date}</Note>
+        </StyledResult>
+    );
+};
 
 export default Result;
--- src/App/Result/styled.js
+++ src/App/Result/styled.js
@@ -1,9 +1,14 @@
 import styled from "styled-components";
 
-export const StyledResult = styled.p`
+export const StyledResult = styled.div`
     font-size: 25px;
 
     @media(max-width: ${({ theme }) => theme.breakpoint.mobileMax}px) {
         font-size: 18px;
     }
+`;
+
+export const Note = styled.p`
+    font-size: 11px;
+    color: ${({ theme }) => theme.color.silverChalice};
 `;
